68085fb78e91f269a2710b5e9059b4f7ec9cc765
Atakan Cetli <atakancetli5@gmail.com>
2026-05-16 01:55:38 +0300

    Implement standard training and validation loops for DL models

---
+++ utils/train_utils.py
@@ -0,0 +1,26 @@
+import torch
+import numpy as np
+
+def train_one_epoch(model, dataloader, criterion, optimizer, device):
+    model.train()
+    total_loss = 0
+    for x, y in dataloader:
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+        output = model(x)
+        loss = criterion(output, y)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(dataloader)
+
+def validate(model, dataloader, criterion, device):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+            output = model(x)
+            loss = criterion(output, y)
+            total_loss += loss.item()
+    return total_loss / len(dataloader)
