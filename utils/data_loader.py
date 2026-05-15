0a3bc6ea28ebf7354884fc1ae04c409c97f85805
Atakan Cetli <atakancetli5@gmail.com>
2026-05-16 01:53:03 +0300

    Implement SKAB dataset loading with source file tracking

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+import os
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from configs.config import Config
 
 class DataLoader:
@@ -8,9 +10,16 @@ class DataLoader:
         self.dataset_name = dataset_name
         self.config = Config()
         self.scaler = StandardScaler()
+        self.pca = PCA(n_components=self.config.PCA_COMPONENTS)
 
-    def load_raw_data(self, path):
-        pass
+    def load_skab(self, path):
+        all_files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.csv')]
+        data_list = []
+        for f in all_files:
+            df = pd.read_csv(f, sep=';', index_col='datetime', parse_dates=True)
+            df['source_file'] = os.path.basename(f)
+            data_list.append(df)
+        return pd.concat(data_list)
 
     def preprocess(self, data):
         pass
